ff301cb086b0bd433d92b42e30152f76ff321d79
Jason Job <jasonwadejob@gmail.com>
2019-03-31 07:19:43 -0700

    Change BCRYPT_LOG_ROUNDS to int.

--- {{cookiecutter.app_name}}/app/settings.py
+++ {{cookiecutter.app_name}}/app/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-BCRYPT_LOG_ROUNDS = os.environ.get('BCRYPT_LOG_ROUNDS', default=13)
+BCRYPT_LOG_ROUNDS = int(os.environ.get('BCRYPT_LOG_ROUNDS') or 13)
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
