230ce382ab6f24f2b2af81f5a1bc3fd4ab8ce2b5
Jason Job <jasonwadejob@gmail.com>
2019-04-14 16:39:36 -0700

    Add settings testdir, appdir, projectdir.

--- {{cookiecutter.app_name}}/app/settings.py
+++ {{cookiecutter.app_name}}/app/settings.py
@@ -10,14 +10,15 @@ import os
 
 from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join('..', basedir, '.env'))
+appdir = os.path.abspath(os.path.dirname(__file__))
+projectdir = os.path.abspath(os.path.join(appdir, '..'))
+load_dotenv(os.path.join(appdir, '.env'))
 
 ENV = os.environ.get('FLASK_ENV')
 DEBUG = ENV == 'development'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
+    'sqlite:///' + os.path.join(projectdir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 BCRYPT_LOG_ROUNDS = int(os.environ.get('BCRYPT_LOG_ROUNDS') or 13)
 DEBUG_TB_ENABLED = DEBUG
